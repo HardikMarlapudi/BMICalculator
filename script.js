dfca7d557605e445f7faf08ee2fecd56b499ccd3
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-19 20:10:08 -0400

    fixed js code

--- script.js
+++ script.js
@@ -4,28 +4,40 @@ let inputWeight = document.getElementById("inputWeight");
 let totalBMI = document.getElementById("totalBMI");
 
 function submit() {
-    const feet = inputFeet.value;
-    const inches = inputInches.value;
-    const weight = inputWeight.value;
+    const feet = Number(inputFeet.value);
+    const inches = Number(inputInches.value);
+    const weight = Number(inputWeight.value);
+    let category = "";
 
-    totalBMI.innerHTML = (weight / ((feet * 12 + inches) ** 2)) * 703;
-
-    if(totalBMI.innerHTML < 18.5) {
-        totalBMI.innerHTML = "Underweight";
-    } else if(totalBMI.innerHTML >= 18.5 && totalBMI.innerHTML <= 24.9) {
-        totalBMI.innerHTML = "Normal";
-    } else if(totalBMI.innerHTML >= 25 && totalBMI.innerHTML <= 29.9) {
-        totalBMI.innerHTML = "Overweight";
-    } else if(totalBMI.innerHTML >= 30) {
-        totalBMI.innerHTML = "Obese";
-    }
+    // Validation comes first
 
     if (inputFeet.value === "" || inputInches.value === "" || inputWeight.value === "") {
         alert("Please fill in all fields");
         totalBMI.innerHTML = "";
+        return;
     }
 
     if(inputFeet.value >= 10 && inputInches.value >= 10) {
         alert("Please enter a valid height");
+        totalBMI.innerHTML = "";
+        return;
     }
+
+    // BMI formula
+
+    const bmi = (weight / ((feet * 12 + inches) ** 2)) * 703;
+
+    if (bmi <= 18.5) {
+        category = "Underweight";
+    } else if(bmi >= 18.5) {
+        category = "Normal";
+    } else if(bmi >= 25) {
+        category = "Overweight";
+    } else if(bmi >= 30) {
+        category = "Obese";
+    }
+
+    // BMI output
+
+    totalBMI.innerHTML = `Your BMI is ${bmi.toFixed(2)} (${category})`;
 }
